b299f3ad368e223a4583b6c72a240119ef629a3e
Felipe Chagas <felchagas@hotmail.com>
2022-01-18 09:27:55 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace forca
+{ //a - i - e - o r s
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var palavras = File.ReadAllText("dicionario5.txt").Split('\n').ToList();
+            var letras = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            palavras = palavras.Select(x => Util.ObterStringSemAcentosECaracteresEspeciais(x)).ToList();
+            foreach(var letra in letras) {
+                var count = palavras.Aggregate(0,(x,y)=>x+y.Count(l => l.Equals(letra)));
+                Console.WriteLine("{0}:{1}", letra, count);
+            }
+        }
+
+        
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,30 @@
+namespace forca {
+    public static class Util {
+        public static string ObterStringSemAcentosECaracteresEspeciais(string str)
+            {
+            /** Troca os caracteres acentuados por não acentuados **/
+                string[] acentos = new string[] { "ç", "Ç", "á", "é", "í", "ó", "ú", "ý", "Á", "É", "Í", "Ó", "Ú", "Ý", "à", "è", "ì", "ò", "ù", "À", "È", "Ì", "Ò", "Ù", "ã", "õ", "ñ", "ä", "ë", "ï", "ö", "ü", "ÿ", "Ä", "Ë", "Ï", "Ö", "Ü", "Ã", "Õ", "Ñ", "â", "ê", "î", "ô", "û", "Â", "Ê", "Î", "Ô", "Û" };
+                string[] semAcento = new string[] { "c", "C", "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "a", "o", "n", "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "A", "O", "N", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+                
+                for (int i = 0; i < acentos.Length; i++)
+                {
+                    str = str.Replace(acentos[i], semAcento[i]);
+                }
+                /** Troca os caracteres especiais da string por "" **/
+                string[] caracteresEspeciais = { "\\.", ",", "-", ":", "\\(", "\\)", "ª", "\\|", "\\\\", "°" };
+                
+                for (int i = 0; i < caracteresEspeciais.Length; i++)
+                {
+                    str = str.Replace(caracteresEspeciais[i], "");
+                }
+                
+                /** Troca os espaços no início por "" **/
+                str = str.Replace("^\\s+", "");
+                /** Troca os espaços no início por "" **/
+                str = str.Replace("\\s+$", "");
+                /** Troca os espaços duplicados, tabulações e etc por " " **/
+                str = str.Replace("\\s+", " ");
+                return str;
+            }
+    }
+}
